fd75c39458b098868fef6ba34033dafc319f0179
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-10-09 16:36:31 -0400

    update

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("Listening on port: ", PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
